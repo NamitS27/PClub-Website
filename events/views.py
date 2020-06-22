4462d0d3742817a6258c610a40711a1f77e4d408
sp2605 <suhanee.p@ahduni.edu.in>
2020-06-22 20:00:20 +0530

    pop-up box

--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Events
 from datetime import datetime
+#from datetime import date
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
