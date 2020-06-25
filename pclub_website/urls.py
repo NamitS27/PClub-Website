65194663ff4f75a8f1c562a2a34c42fb0be18a18
Namit Shah <namit.s@ahduni.edu.in>
2020-06-25 12:15:57 +0530

    home responsive

--- pclub_website/urls.py
+++ pclub_website/urls.py
@@ -6,13 +6,11 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/',include("events.urls")),
-    # path('summernote/',include("django_summernote.urls")),
     path('resources/',include("resource.urls")),
     path('contact_us/',include("contact_us.urls")),
     path('about_us/',include("about_us.urls")),
     path('cp/',include("competitive_programming.urls")),
-    path('home/',include("home_announce.urls")),
-    #path('testing/',include("testing.urls")),
+    path('',include("home_announce.urls")),
 ]
 
 if settings.DEBUG:
