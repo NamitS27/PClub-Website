c0d75986fc5ce6685daaabd08e4df02ff1ed5115
Namit Shah <namit.s@ahduni.edu.in>
2020-07-02 12:04:01 +0530

    events changes

--- events/models.py
+++ events/models.py
@@ -5,7 +5,7 @@ from datetime import *
 class Events(models.Model):
     name = models.CharField(max_length=200,null=False)
     description = models.TextField()
-    logo = models.ImageField(default=None,blank=True,upload_to="images/")
+    logo = models.ImageField(default='default.png')
     date = models.DateTimeField(default=None)
     can_register = models.BooleanField(default=False, blank=True)
     feedback_link = models.URLField(max_length=200,blank=True,default=None)
