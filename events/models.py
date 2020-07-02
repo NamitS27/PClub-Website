1d5603148ff8aea1c4a3e63f2197272df9f56072
Namit Shah <namit.s@ahduni.edu.in>
2020-07-02 12:17:19 +0530

    event feild changes

--- events/models.py
+++ events/models.py
@@ -8,7 +8,7 @@ class Events(models.Model):
     logo = models.ImageField(default='default.png')
     date = models.DateTimeField(default=None)
     can_register = models.BooleanField(default=False, blank=True)
-    feedback_link = models.URLField(max_length=200,blank=True,default=None)
+    # feedback_link = models.URLField(max_length=200,blank=True,default=None)
     photos_link = models.URLField(max_length=200,blank=True)
     registration_date = models.DateTimeField(default=None,blank=True,null=True)
     registration_link = models.URLField(max_length=200,blank=True) 
